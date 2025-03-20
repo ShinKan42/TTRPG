e450f9037ff5bdf306394268127dcf6832e91937
ShinKan42 <ShinKan@foxmail.com>
2025-03-20 21:06:36 +0800

    Analytics

--- Vue-Press-Plume/docs/.vuepress/theme/components/Custom.vue
+++ Vue-Press-Plume/docs/.vuepress/theme/components/Custom.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const message = ref('Hello World!')
+import { ref } from "vue";
+import { Analytics } from "@vercel/analytics/vue";
+const message = ref("Hello World!");
 </script>
 
 <template>
-  <div class="my-custom-content">
-    {{ message }}
-  </div>
+	<Analytics />
+	<div class="my-custom-content">
+		{{ message }}
+	</div>
 </template>
