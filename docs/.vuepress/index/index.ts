86c3557d7676b917decae5f976a6987c2e828054
ShinKan42 <ShinKan@foxmail.com>
2025-10-09 08:59:42 +0800

    debug 2

--- docs/.vuepress/index/index.ts
+++ docs/.vuepress/index/index.ts
@@ -1,16 +1,16 @@
 import { defineCollections } from 'vuepress-theme-plume';
 
-import COC from './rule/coc';
-import DND5R from './rule/dnd5r';
-import CyberpunkRed from './rule/cyberpunkred';
-import Avatar from './rule/avatar';
+import COC from './rule/coc.ts';
+import DND5R from './rule/dnd5r.ts';
+import CyberpunkRed from './rule/cyberpunkred.ts';
+import Avatar from './rule/avatar.ts';
 
-import Modules from './modules';
+import Modules from './modules.ts';
 
-import About from './about';
+import About from './about.ts';
 
-import PL_The_Show_Must_Go_On from './pl_ref_index/the_show_must_go_on';
-import GM_The_Show_Must_Go_On from './gm_ref_index/the_show_must_go_on';
+import PL_The_Show_Must_Go_On from './pl_ref_index/the_show_must_go_on.ts';
+import GM_The_Show_Must_Go_On from './gm_ref_index/the_show_must_go_on.ts';
 
 export default defineCollections([
 	COC,
