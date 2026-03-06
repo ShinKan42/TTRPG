8c18aea98eb272086965b86baacfb17cd2f99cdd
ShinKan42 <ShinKan@foxmail.com>
2026-03-06 12:15:36 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -20,6 +20,21 @@ export default defineUserConfig({
 
 		hostname: 'http://ttrpg.shinkan42.art/',
 
+   		 head: [
+   		   // Google验证（直接粘贴从GSC复制的完整标签）
+  		    ['meta', { 
+  		      name: 'google-site-verification', 
+  		      content: '<meta name="google-site-verification" content="dAVp0f72P9QitJLdUdLt9xLrQoNKqTsYCGS_SJMH3AI" />' 
+  		    }],
+      
+  		    // 百度验证（自己组装，content值替换为你从百度获取的）
+ 		     ['meta', { 
+ 		       name: 'baidu-site-verification', 
+ 		       content: 'codeva-qsgrSAbOG4' 
+		      }],
+  		  ],
+
+
 		// 默认 不启用，仅当 plugins.git 为 true 时生效
 		// 此配置在 plume.config.ts 中无效
 		changelog: {
