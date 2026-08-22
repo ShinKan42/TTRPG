0adaf68180ccb852037c8a514648beb01aa28995
ShinKan42 <ShinKan@foxmail.com>
2026-08-23 00:08:55 +0800

    deploy: site update

--- docs/.vuepress/index/prep_doomed_sword_coast.ts
+++ docs/.vuepress/index/prep_doomed_sword_coast.ts
@@ -6,13 +6,16 @@ export default defineCollection({
 	dir: 'prep/dnd_5r/doomed_sword_coast',
 	sidebar: [
 		'index',
-		'academy_of_adventure',
-		'vecna_rise',
-		'vecna_fall',
-		'legacy_of_vecna',
-		'reign_of_rot',
-		'wrath_of_zuggtmoy',
-		'blood_war',
+		{
+			text: '⚔️ 主线：维克那四部曲',
+			prefix: '/',
+			items: ['academy_of_adventure', 'vecna_rise', 'vecna_fall', 'legacy_of_vecna'],
+		},
+		{
+			text: '🕳️ 末日幽暗地域线',
+			prefix: '/',
+			items: ['reign_of_rot', 'wrath_of_zuggtmoy', 'blood_war'],
+		},
 		'geography',
 	],
 });
