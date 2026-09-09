529d3ba57d2e6826f3c0ab4f1b1d2fc52335c6a8
ShinKan42 <ShinKan@foxmail.com>
2026-09-09 18:50:10 +0800

    DH资料区:简介推荐页按资料选择页骨架律重写(FAQ/核心简介/挑选问卷/跳转更多资料)+侧栏跳转组嵌套改名

--- docs/.vuepress/index/rule/dagger_heart.ts
+++ docs/.vuepress/index/rule/dagger_heart.ts
@@ -25,12 +25,18 @@ export default defineCollection({
 			collapsed: true,
 			items: [
 				{ text: '简介推荐 ⭐', link: '/rule/dagger_heart/brief_ref/' },
-				{ text: '规则速查 ⚡', link: 'https://daggerheart.huijiwiki.com/wiki/%E8%A7%84%E5%88%99%E9%80%9F%E6%9F%A5' },
-				{ text: '职业 🗡️', link: 'https://daggerheart.huijiwiki.com/wiki/%E5%88%86%E7%B1%BB:%E8%81%8C%E4%B8%9A' },
-				{ text: '种族 🧝', link: 'https://daggerheart.huijiwiki.com/wiki/%E5%88%86%E7%B1%BB:%E7%A7%8D%E6%97%8F' },
-				{ text: '社群 🏘️', link: 'https://daggerheart.huijiwiki.com/wiki/%E5%88%86%E7%B1%BB:%E7%A4%BE%E7%BE%A4' },
-				{ text: '领域 📜', link: 'https://daggerheart.huijiwiki.com/wiki/%E9%A2%86%E5%9F%9F%E5%8D%A1' },
-				{ text: '装备 🎒', link: 'https://daggerheart.huijiwiki.com/wiki/%E8%A3%85%E5%A4%87' },
+				{
+					text: '跳转更多资料 🔗',
+					collapsed: true,
+					items: [
+						{ text: '规则速查 ⚡', link: 'https://daggerheart.huijiwiki.com/wiki/%E8%A7%84%E5%88%99%E9%80%9F%E6%9F%A5' },
+						{ text: '职业 🗡️', link: 'https://daggerheart.huijiwiki.com/wiki/%E5%88%86%E7%B1%BB:%E8%81%8C%E4%B8%9A' },
+						{ text: '种族 🧝', link: 'https://daggerheart.huijiwiki.com/wiki/%E5%88%86%E7%B1%BB:%E7%A7%8D%E6%97%8F' },
+						{ text: '社群 🏘️', link: 'https://daggerheart.huijiwiki.com/wiki/%E5%88%86%E7%B1%BB:%E7%A4%BE%E7%BE%A4' },
+						{ text: '领域 📜', link: 'https://daggerheart.huijiwiki.com/wiki/%E9%A2%86%E5%9F%9F%E5%8D%A1' },
+						{ text: '装备 🎒', link: 'https://daggerheart.huijiwiki.com/wiki/%E8%A3%85%E5%A4%87' },
+					],
+				},
 			],
 		},
 		{
