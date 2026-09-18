76af861de96dbb27e838e053671cf96419bf2834
ShinKan42 <ShinKan@foxmail.com>
2026-09-18 23:12:27 +0800

    我们搁置之事侧栏子项补右置emoji

--- docs/.vuepress/index/prep_coc_ttwlb.ts
+++ docs/.vuepress/index/prep_coc_ttwlb.ts
@@ -9,16 +9,16 @@ export default defineCollection({
 			text: '📰 全球新闻社',
 			collapsed: false,
 			items: [
-				{ text: '组织页', link: 'global_news_service' },
-				{ text: '模组之后', link: 'global_news_service_aftermath' },
+				{ text: '组织页 🏛️', link: 'global_news_service' },
+				{ text: '模组之后 🔮', link: 'global_news_service_aftermath' },
 			],
 		},
 		{
 			text: '🐞 模组详情',
 			collapsed: false,
 			items: [
-				{ text: '瓢虫，瓢虫，飞回家', link: 'ladybug_ladybug_fly_away_home' },
-				{ text: '德州地狱', link: 'hell_in_texas' },
+				{ text: '瓢虫，瓢虫，飞回家 🐞', link: 'ladybug_ladybug_fly_away_home' },
+				{ text: '德州地狱 🤠', link: 'hell_in_texas' },
 			],
 		},
 	],
