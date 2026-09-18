51a81a5a3238f85a318a57fc620e31a87399bb51
ShinKan42 <ShinKan@foxmail.com>
2026-09-18 23:59:39 +0800

    player_option collection ts改名dnd_5r_player_option.ts+import同步——文件名前缀匹配同级dnd_5r.ts命名(带下划线),防与其他规则集同名ts混淆

--- docs/.vuepress/index/index.ts
+++ docs/.vuepress/index/index.ts
@@ -2,7 +2,7 @@ import { defineCollections } from 'vuepress-theme-plume';
 
 import COC_7 from './rule/coc_7.ts';
 import Dagger_Heart from './rule/dagger_heart.ts';
-import DND_PlayerOption from './rule/player_option.ts';
+import DND_5R_PlayerOption from './rule/dnd_5r_player_option.ts';
 import DND_5R from './rule/dnd_5r.ts';
 import Cyberpunk_Red from './rule/cyberpunk_red.ts';
 import Avatar_Legend from './rule/avatar_legend.ts';
@@ -30,7 +30,7 @@ import Log from './log.ts';
 export default defineCollections([
 	COC_7,
 	Dagger_Heart,
-	DND_PlayerOption,
+	DND_5R_PlayerOption,
 	DND_5R,
 	Cyberpunk_Red,
 	Avatar_Legend,
